96fd6767b36fecbcdff9b95ef479cde207e31e16
Michael Bell <marvinpa97@shaw.ca>
2015-02-26 20:11:38 -0800

    Start trail Collide logic

---
+++ core/src/ca/michaelbell/gameobjects/collisionHandler.java
@@ -0,0 +1,7 @@
+package ca.michaelbell.gameobjects;
+
+/**
+ * Created by 788732 on 25/02/2015.
+ */
+public class collisionHandler {
+}
--- core/src/ca/michaelbell/gameworld/GameRenderer.java
+++ core/src/ca/michaelbell/gameworld/GameRenderer.java
@@ -1,12 +1,17 @@
 package ca.michaelbell.gameworld;
 
 import ca.michaelbell.gameobjects.Player;
+import ca.michaelbell.gameobjects.Square;
 import ca.michaelbell.helpers.AssetLoader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Michael on 2015-01-31.
@@ -20,6 +25,7 @@ public class GameRenderer {
 
     private int midPointY;
     private int gameHeight;
+    private Iterator<Square> itr; // iterator to go through line segments when rendering
 
     public GameRenderer(GameWorld world, int gameWidth,  int gameHeight, int midPointY) {
         myWorld = world;
@@ -35,8 +41,8 @@ public class GameRenderer {
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(cam.combined);
         player = myWorld.getPlayer(); // who is that again?
-
     }
+
 
     public void render(float runTime) {
         // Fill the entire screen with black, to prevent potential flickering.
@@ -44,11 +50,15 @@ public class GameRenderer {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
-        player.getTrail().render(shapeRenderer); // render the trail from the trail method
 
         // Begin SpriteBatch
         batcher.begin();
-        batcher.draw(AssetLoader.playerTexture, player.getX(), player.getY(), player.getWidth(), player.getHeight()); // Draw the player... He probably could be a rectangle... Oh well
+        itr = myWorld.getSquareList().iterator();
+        while (itr.hasNext()) { // iterator
+            Square square = itr.next();
+            square.getTrail().render(shapeRenderer, batcher); // render the trail from the trail method
+            batcher.draw(AssetLoader.playerTexture, square.getX(), square.getY(), square.getWidth(), square.getHeight()); // Draw the player... He probably could be a rectangle... Oh well
+        }
         batcher.end();
     }
 }
